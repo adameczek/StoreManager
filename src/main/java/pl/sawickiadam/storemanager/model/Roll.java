f674e08badb667ec7654bee017a0694b7e97dc01
Adam Sawicki <adam2sawicki@gmail.com>
2020-05-06 20:18:22 +0200

    Add user and roll classes

---
+++ src/main/java/pl/sawickiadam/storemanager/model/Roll.java
@@ -0,0 +1,45 @@
+package pl.sawickiadam.storemanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roll")
+public class Roll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int colorNumber;
+    @Column(nullable = false)
+    private int numberOfRolls;
+
+    public Roll() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getColorNumber() {
+        return colorNumber;
+    }
+    public void setColorNumber(int colorNumber) {
+        this.colorNumber = colorNumber;
+    }
+
+    public int getNumberOfRolls() {
+        return numberOfRolls;
+    }
+    public void setNumberOfRolls(int numberOfRolls) {
+        this.numberOfRolls = numberOfRolls;
+    }
+
+
+
+}
---
+++ src/main/java/pl/sawickiadam/storemanager/model/User.java
@@ -0,0 +1,34 @@
+package pl.sawickiadam.storemanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
